0d3940c99c23b6d70ca0bcdc69b7b5ddd88e8ecd
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2024-02-02 00:14:55 -0600

    Render Card component to use FlatList

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,15 +1,49 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import Card from "../components/Card";
+import { getNews } from "../services/api";
 
 function Home({ navigation }) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    getNews()
+      .then((response) => setData(response))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const { articles } = data;
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Card
+        article={item}
+        navigation={navigation}
+      />
+    </View>
+  );
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Details")}>
-        <Text>Ver Detalles</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 10,
+  },
+  item: {
+    marginBottom: 10,
+  },
+});
 
 export default Home;
