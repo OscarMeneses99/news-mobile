7d8ae7de1e0c4126b862b7ac8ac657883495ec61
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2024-02-23 19:40:58 -0600

    Refactor Home screen component

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { View } from "react-native";
-import useFetch from "../hooks/useFetch.js";
+import React, { useContext } from "react";
+import { View, Text } from "react-native";
+import Search from "../components/Search.jsx";
 import Loading from "../components/Loading.jsx";
 import ListOfNews from "../components/ListOfNews.jsx";
 import Error from "../components/Error.jsx";
+import { NewsContext } from "../context/Context.jsx";
 
 function Home({ navigation }) {
-  const { data, loading, error } = useFetch();
+  const { data, loading, error, search } = useContext(NewsContext);
+
   const { articles } = data;
   return (
     <View>
+      <Search />
+      {articles?.length === 0 && (
+        <Text style={{ textAlign: "center", marginTop: 15 }}>
+          No results found for
+          <Text style={{ fontWeight: "bold" }}> {search}</Text>
+        </Text>
+      )}
+
       {loading ? (
         <Loading />
       ) : (
